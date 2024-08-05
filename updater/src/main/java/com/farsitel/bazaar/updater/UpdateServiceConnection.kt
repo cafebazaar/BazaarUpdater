3ff973401ad448d369cd53f2132006388d417ddd
Hamid Mahmoodi <shr.mahmoodi@gmail.com>
2024-08-05 17:50:59 +0330

    Check bazaar version for aidl

--- updater/src/main/java/com/farsitel/bazaar/updater/UpdateServiceConnection.kt
+++ updater/src/main/java/com/farsitel/bazaar/updater/UpdateServiceConnection.kt
@@ -4,9 +4,14 @@ import android.content.ComponentName
 import android.content.ServiceConnection
 import android.os.IBinder
 import com.farsitel.bazaar.IUpdateCheckService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class UpdateServiceConnection(
+internal class UpdateServiceConnection(
     private val packageName: String,
+    private val scope: CoroutineScope,
+    private val bazaarVersionCode: Long,
     private val onResult: ((Long) -> Unit),
     private val onError: ((String) -> Unit),
 ) : ServiceConnection {
@@ -16,8 +21,14 @@ class UpdateServiceConnection(
     override fun onServiceConnected(name: ComponentName?, boundService: IBinder?) {
         service = IUpdateCheckService.Stub.asInterface(boundService)
         try {
-            val versionCode = service?.getVersionCode(packageName)
-            versionCode?.let { onResult(it) }
+            scope.launch(Dispatchers.IO) {
+                val versionCode = if (bazaarVersionCode >= BAZAAR_CODE_REMOTE_VERSION_SUPPORTED) {
+                    service?.getRemoteVersionCode(packageName)
+                } else {
+                    service?.getVersionCode(packageName)
+                }
+                versionCode?.let { onResult(it) }
+            }
         } catch (e: Exception) {
             onError(e.message.orEmpty())
         }

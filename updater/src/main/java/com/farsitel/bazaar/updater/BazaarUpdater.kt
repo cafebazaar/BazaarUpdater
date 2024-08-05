b723b781bb3e4d483db8636efaf3f628e079d2d7
Hamid Mahmoodi <shr.mahmoodi@gmail.com>
2024-08-05 17:21:28 +0330

    Add lifeCycleScope

--- updater/src/main/java/com/farsitel/bazaar/updater/BazaarUpdater.kt
+++ updater/src/main/java/com/farsitel/bazaar/updater/BazaarUpdater.kt
@@ -3,8 +3,13 @@ package com.farsitel.bazaar.updater
 import android.content.Context
 import android.content.Intent
 import androidx.core.net.toUri
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
 import com.farsitel.bazaar.updater.Security.verifyBazaarIsInstalled
 import com.farsitel.bazaar.updater.VersionParser.parseUpdateResponse
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
 import java.lang.ref.WeakReference
 
 object BazaarUpdater {
@@ -13,12 +18,20 @@ object BazaarUpdater {
 
     fun getLastUpdateState(
         context: Context,
+        scope: CoroutineScope? = null,
         onResult: (UpdateResult) -> Unit
     ) {
         if (verifyBazaarIsInstalled(context).not()) {
             onResult(UpdateResult.Error(ERROR_BAZAAR_IS_NOT_INSTALL))
         } else {
-            initService(context, onResult)
+            initService(
+                context = context,
+                onResult = onResult,
+                scope = retrieveScope(
+                    context = context,
+                    scope = scope
+                )
+            )
         }
     }
 
@@ -35,10 +48,28 @@ object BazaarUpdater {
         context.startActivity(intent)
     }
 
-    private fun initService(context: Context, onResult: (UpdateResult) -> Unit) {
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun retrieveScope(
+        context: Context,
+        scope: CoroutineScope?
+    ): CoroutineScope {
+        if (scope != null) return scope
+        return if (context is LifecycleOwner) {
+            context.lifecycleScope
+        } else {
+            GlobalScope
+        }
+    }
+
+    private fun initService(
+        context: Context,
+        scope: CoroutineScope,
+        onResult: (UpdateResult) -> Unit
+    ) {
         connection = WeakReference(
             UpdateServiceConnection(
                 packageName = context.packageName,
+                scope = scope,
                 onResult = { updateVersion ->
                     onResult(parseUpdateResponse(version = updateVersion, context = context))
                     releaseService(context)

eea4e75a2e06d151852c898506d1a6368ce475e6
AmirHosseinAghajari <amirhisein82.aa@gmail.com>
2024-09-07 18:31:22 +0330

    Refactor sample app

--- app/src/main/java/com/farsitel/bazaar/bazaarupdaterSample/ui/theme/Theme.kt
+++ app/src/main/java/com/farsitel/bazaar/bazaarupdaterSample/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.farsitel.bazaar.bazaarupdaterSample.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -34,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun BazaarupdaterSampleTheme(
+fun BazaarUpdaterSampleTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
